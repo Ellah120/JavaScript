ebf8ff80519df3384b805e6ebad7e8d73ee72049
unknown <nwokeellah88@gmail.com>
2022-06-08 08:33:44 +0100

    first commit

---
+++ ifelse.js
@@ -0,0 +1,30 @@
+// let year = 2021;
+// function whatYear(number){
+//   if(number == 2022){
+//     console.log("You are right");
+//   }else {
+//     console.log("How can you be so wrong?"); 
+// }
+// }
+// whatYear(year);
+
+// let age = 60;
+
+// if(50 > age && age > 40) {
+//   console.log("You are still strong");
+// } else if(40 > age && age > 30){
+//   console.log("keep on balling");
+// } else {
+//   console.log("You're ok");
+// }
+
+
+let browser = "Mode";
+
+if(browser == "Edge") {
+  console.log("You've got the Edge!");
+} else if(browser == "Chrome" || "Firefox" || "Safari" || "Opera") {
+console.log("Okay we support these browsers too");
+} else {
+  console.log("We hope that this page looks okay");
+}
